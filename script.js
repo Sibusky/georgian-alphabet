320f7e0ca4c90c89024b7f6cb415c65ccce54d06
Alexey Smirnov <Sibusky@yandex.ru>
2022-10-16 11:09:17 +0400

    feat: solve main task to replace all charachters correctly. Next step to improve layout

--- script.js
+++ script.js
@@ -4,65 +4,13 @@ const textDone = document.querySelector('.transliteration__transliterated-text')
 const buttonTransliteration = document.querySelector('.transliteration__button-transliteration');
 const buttonReset = document.querySelector('.transliteration__button-reset');
 
-// checkboxInputs.forEach((item) => {
-//     if (item.checked) {
-//         console.log(item.value)
-//     }
-// });
-
+// Функция сброса текста
 function resetText() {
     text.value = "";
     textDone.value = "";
 };
 
-const characterEquivalent = {
-    "ა": "а",
-    "ბ": "б",
-    "გ": "г",
-    "დ": "д",
-    "ე": "е",
-    "ვ": "в",
-    "ზ": "з",
-    "თ": "тэ",
-    "ი": "и",
-    "კ": "кэ",
-    "ლ": "л",
-    "მ": "м",
-    "ნ": "н",
-    "ო": "о",
-    "პ": "п",
-    "ჟ": "ж",
-    "რ": "р",
-    "ს": "с",
-    "ტ": "т",
-    "უ": "у",
-    "ფ": "пэ",
-    "ქ": "к",
-    "ღ": "гэ",
-    "ყ": "кʼ",
-    "შ": "ш",
-    "ჩ": "ч",
-    "ც": "тс",
-    "ძ": "дз",
-    "წ": "ц",
-    "ჭ": "чʼ",
-    "ხ": "х",
-    "ჯ": "дж",
-    "ჰ": "хʼ",
-};
-
-// Функция для замены ключей и значений местами
-function swap(obj) {
-    const res = {};
-    Object.keys(obj).forEach(function (value) {
-        let key = obj[value];
-        res[key] = value;
-    });
-    return res;
-}
-
-
-// Функция поиска и замены
+// Функция поиска и замены букв
 function findAndReplace(inputText, characterObj) {
     let outputText = "";
 
@@ -71,48 +19,15 @@ function findAndReplace(inputText, characterObj) {
     };
 
     characterObj.forEach((item) => {
-        for (let i = 0; i <= inputText.length; i++) {
-            if (inputText[i] === item.value && item.checked) {
-                inputText = inputText.replaceAt(i, item.name)
+        for (let i = 0; i < inputText.length; i++) {
+            if (inputText[i].toLowerCase() === item.value && item.checked) {
+                inputText = inputText.replaceAt(i, item.name);
             };
         };
     });
     outputText = inputText;
     return outputText;
-}
-
-
-
-
-
-
-
-// // Функция поиска и замены
-// function findAndReplace(inputText, characterObj) {
-//     let outputText = "";
-
-//     String.prototype.replaceAt = function (index, character) {
-//         return this.substring(0, index) + character + this.substring(index + character.length);
-//     }
-
-//     Object.values(characterObj).forEach((item, index) => {
-//         for (let i = 0; i <= inputText.length; i++) {
-//             if (inputText[i] === item) {
-//                 inputText = inputText.toLowerCase().replaceAt(i, Object.keys(characterObj)[index])
-//             }
-//         }
-//     })
-//     outputText = inputText;
-//     return outputText;
-// }
-
-// const transliteratedText = findAndReplace(text.textContent, checkboxInputs);
-
-// console.log(transliteratedText)
-
-// function replaceText () {
-//     textDone.value = transliteratedText;
-// }
+};
 
 buttonReset.addEventListener('click', resetText);
 buttonTransliteration.addEventListener('click', () => {
