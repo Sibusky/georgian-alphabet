00497ec4ff40ef9adb758d1e0f5cc9464d2e8a26
Alexey Smirnov <Sibusky@yandex.ru>
2023-01-23 19:23:42 +0400

    feat: realize ability to choose only one chararter from list of similar

--- script.js
+++ script.js
@@ -33,14 +33,16 @@ function findAndReplace(inputText, characterArr) {
       if (
         inputText[i].toLowerCase() === 'д' &&
         inputText[i + 1].toLowerCase() === 'з' &&
-        inputText[i].toLowerCase() + inputText[i + 1].toLowerCase() === item.value &&
+        inputText[i].toLowerCase() + inputText[i + 1].toLowerCase() ===
+          item.value &&
         item.checked
       ) {
         inputText = inputText.replaceAt(i, i + 1, item.name);
       } else if (
         inputText[i].toLowerCase() === 'д' &&
         inputText[i + 1].toLowerCase() === 'ж' &&
-        inputText[i].toLowerCase() + inputText[i + 1].toLowerCase() === item.value &&
+        inputText[i].toLowerCase() + inputText[i + 1].toLowerCase() ===
+          item.value &&
         item.checked
       ) {
         inputText = inputText.replaceAt(i, i + 1, item.name);
@@ -65,9 +67,47 @@ function findAndReplace(inputText, characterArr) {
   return outputText;
 }
 
+// Функция выбора только одной буквы из ряда похожих
+function chooseOnlyOne(array) {
+  array.map((item) => {
+    let arrWithoutItem = array.filter((i) => i !== item);
+    item.addEventListener('click', () => {
+      if (item.checked !== false) {
+        arrWithoutItem.map((i) => (i.checked = false));
+      }
+    });
+  });
+}
+
+const oneK = chooseOnlyOne([
+  checkboxInputs[9],
+  checkboxInputs[21],
+  checkboxInputs[23],
+]);
+const oneP = chooseOnlyOne([checkboxInputs[14], checkboxInputs[20]]);
+const oneT = chooseOnlyOne([checkboxInputs[7], checkboxInputs[18]]);
+const oneH = chooseOnlyOne([checkboxInputs[30], checkboxInputs[32]]);
+const oneCh = chooseOnlyOne([checkboxInputs[25], checkboxInputs[29]]);
+const oneTs = chooseOnlyOne([checkboxInputs[26], checkboxInputs[28]]);
+
 // Слушатели событий
 buttonCheckAll.addEventListener('click', () => {
-  checkboxInputs.forEach((item) => (item.checked = true));
+  checkboxInputs.forEach((item) => {
+    if (
+      !(
+        item === checkboxInputs[21] ||
+        item === checkboxInputs[23] ||
+        item === checkboxInputs[20] ||
+        item === checkboxInputs[18] ||
+        item === checkboxInputs[32] ||
+        item === checkboxInputs[29] ||
+        item === checkboxInputs[28]
+      )
+    ) {
+      // console.log(item);
+      item.checked = true;
+    }
+  });
 });
 
 buttonCheckNone.addEventListener('click', () => {
